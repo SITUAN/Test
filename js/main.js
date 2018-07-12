35bb7a7c7f6692c9a9b049557eb0c41a3f6e5e0d
tuanho <hosytuan95@gmail.com>
2018-07-12 11:03:00 +0700

    add code

---
+++ js/main.js
@@ -0,0 +1,90 @@
+jQuery(document).ready(function($){
+
+	 // modal social
+    $(".show-mb").click(function(){
+        $(".menu").addClass("menushow");
+        $(this).removeClass("menu").addClass("menushow");
+    });
+
+    // hiden modal social
+    $(".menu_close").click(function(){
+        $(".menu").removeClass("menushow");
+    });
+    
+	 $('.slide').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    dots: false,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        },
+	        1200:{
+	            items:1
+	        }
+	    },
+	    navText: ["<i class='left-icon-m'></i>","<i class='right-icon-m'></i>"],
+	});
+	
+	$('.sli-blog').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    nav:true,
+	    dots: false,
+	    responsive:{
+	        0:{
+	            items:2
+	        },
+	        600:{
+	            items:4
+	        },
+	        1000:{
+	            items:6
+	        },
+	        1200:{
+	            items:6
+	        }
+	    },
+	    navText: ["<i class='left-icon-m sub-slider'></i>","<i class='right-icon-m r-sub-slider'></i>"],
+	});
+
+	$('.sli-hieu').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    nav:true,
+	    dots: false,
+	    responsive:{
+	        0:{
+	            items:2
+	        },
+	        600:{
+	            items:4
+	        },
+	        1000:{
+	            items:6
+	        },
+	        1200:{
+	            items:6
+	        }
+	    },
+	    navText: ["<i class='left-icon-m sub-slider'></i>","<i class='right-icon-m r-sub-slider'></i>"],
+	});
+	
+    $(".tuan li").click(function(e){
+        if (!$(this).hasClass("active")) {
+            var tabNum = $(this).index();
+            var nthChild = tabNum+1;
+            $(".tuan li.active").removeClass("active");
+            $(this).addClass("active");
+            $(".tuanho li.active").removeClass("active");
+            $(".tuanho li:nth-child("+nthChild+")").addClass("active");
+        }
+    });
+});
